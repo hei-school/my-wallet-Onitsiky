81ebc978a18b9070273250ec1bb5824ee4da63af
Onitsiky <hei.onitsiky@gmail.com>
2023-12-07 16:29:45 +0300

    feat: cards operations

---
+++ wallet/card.py
@@ -0,0 +1,102 @@
+from transaction import make_transactions
+cards = []
+
+class Card:
+    def __init__(self, number, name, balance):
+        self.number = number
+        self.name = name
+        self.balance = balance
+    
+    def display_card(self):
+        print(f"La carte {self.name}, ayant le numéro {self.number} a actuellement en solde: {self.balance} ariary.")
+
+def handle_cards():
+    card_menu = "    Choisissez une action à faire: \n      1. Ajouter une carte\n      2. Retirer une carte\n      3. Voir les cartes\n      4. Recharger une carte\n      5. Débiter une carte\n Votre choix: "
+    wrong_choice = "\n Choix invalide. Veuillez choisir un choix parmi ceux mentionnés dans le menu.\n"
+    valid_choices = [1, 2, 3, 4, 5]
+    choice = int(input(card_menu))
+    if  choice not in valid_choices:
+        print(wrong_choice)
+        handle_cards()
+    elif choice == 1:
+        add_card()
+    elif choice == 2:
+        remove_card()
+    elif choice == 3:
+        list_cards()
+    elif choice == 4:
+        refill_card()
+    elif choice == 5:
+        debit_card()
+
+
+def add_card():
+    number = input("    Veuillez entrer le numéro de votre carte: ")
+    name = input("    Veuillez entrer le nom de votre carte: ")
+    balance = int(input("    Veuillez entrer le montant du solde de votre carte: "))
+    if(balance < 0):
+        print("\n  Le montant de votre retrait devrait-ếtre une valeur positive, différente de 0.\n")
+        add_card()
+    else:
+        cards.append(Card(number=number, name=name, balance=balance))
+        print("\n  Carte ajoutée avec succès.\n")
+        from menu import show_menu
+        show_menu()
+
+def remove_card():
+    global cards
+    to_remove = input("\n    Veuillez renseigner le numéro de carte à retirer: ")
+    exists = any(item.number == to_remove for item in cards)
+    if not exists:
+        print("\n    La carte spécifiée n'existe pas.")
+        remove_card()
+    else:
+        cards = [card for card in cards if card.number.lower() != to_remove.lower()]
+        print("\n    Carte retirée avec succès.\n")
+        from menu import show_menu
+        show_menu()
+
+def list_cards():
+    for item in cards:
+        print(f"{item.display_card()}\n")
+        from menu import show_menu
+        show_menu()
+
+def refill_card():
+    global cards
+    number = input("    Veuillez entrer le numéro de votre carte: ")
+    exists = any(item.number == number for item in cards)
+    if not exists:
+        print("\n    La carte spécifiée n'existe pas.")
+        refill_card()
+    else:
+        amount = int(input("    Veuillez entrer le montant à ajouter: "))
+        if(amount < 0):
+            print("\n  Vous avez entré une valeur invalide. Veuiller entre une valeur supérieure à 0.\n")
+            refill_card()
+        else:
+            cards = [Card(card.number, card.name, card.balance + amount) if card.number.lower() == number.lower() else card for card in cards]
+            print("\n    Balance de la carte mise à jour avec succès.\n")
+            make_transactions(f"recharge sur la carte {number}", amount=amount)
+            from menu import show_menu
+            show_menu()
+    
+
+def debit_card():
+    global cards
+    number = input("    Veuillez entrer le numéro de votre carte: ")
+    exists = any(item.number == number for item in cards)
+    if not exists:
+        print("\n    La carte spécifiée n'existe pas.")
+        debit_card()
+    else:
+        amount = int(input("\    Veuillez entrer le montant à débiter: "))
+        if(amount < 0):
+            print("\n  Vous avez entré une valeur invalide. Veuiller entre une valeur supérieure à 0.\n")
+            refill_card()
+        else:
+            cards = [Card(card.number, card.name, card.balance - amount) if card.number.lower() == number.lower() else card for card in cards]
+            print(f"\n    Montant de {amount} débitée avec succès.\n")
+            make_transactions(f"retrait sur la carte {number}", amount=amount)
+            from menu import show_menu
+            show_menu()
--- wallet/menu.py
+++ wallet/menu.py
@@ -1,5 +1,6 @@
 from cash import get_balance, deposit, withdraw
 from transaction import handle_trasaction
+from card import handle_cards
 
 def show_menu():
     menu = f"\n  Bienvenue sur My Wallet, votre application de gestion de porte-feuilles.\n     Votre solde actuel est de {get_balance()} ariary.\n     Choisissez une action à faire: \n      1. Déposer de l'argent\n      2. Retirer de l'argent \n      3. Gérer les transactions \n      4. Gérer les cartes \n      5. Quitter l'application \nVotre choix: "
@@ -15,5 +16,7 @@ def show_menu():
         withdraw()
     elif choice == 3:
         handle_trasaction()
+    elif choice == 4:
+        handle_cards()
     elif choice == 5:
         print("Merci de votre confiance, à la prochaine !")
